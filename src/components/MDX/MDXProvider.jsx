754380ef8d5a8e2e486b428590291c11bf49f465
Sergio Trejo <alejandrotrejocode@gmail.com>
2020-09-05 13:40:59 -0500

    ⚠📄 Navegación completa - Aplicación de MDX para el proyecto - Primera fracción del index.mdx

--- src/components/MDX/MDXProvider.jsx
+++ src/components/MDX/MDXProvider.jsx
@@ -1,10 +1,28 @@
+/**
+ * @version 1.0.1
+ * @author Trejocode - Sergio
+ * @description Provedor de características de MDX
+*/
+
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
+import CodeBlock from '../MDX/CodeBlock';
 
-const Provider = ({ children }) => (
-    <MDXProvider>
-        { children }
-    </MDXProvider>
-);
+const Provider = ({ children }) => {
+
+    const components = {
+        pre: props => <div {...props} />,
+        code: CodeBlock
+    };
+
+    return(
+        <MDXProvider components={components}>
+            <main>
+                { children }
+            </main>
+        </MDXProvider>
+    );
+
+};
 
 export default Provider;
--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -15,8 +15,18 @@ const Aside = () => {
                     <li className="level active">
                         📌 Introducción
                     </li>
-                    <li className="level">
-                        📝 Implementación
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                📝 Implementación
+                            </li>
+                            <li className="sublevel">
+                                CSS
+                            </li>
+                            <li className="sublevel">
+                                SASS
+                            </li>
+                        </ul>
                     </li>
                     <li className="level-container">
                         <ul>
@@ -37,6 +47,111 @@ const Aside = () => {
                             </li>
                             <li className="sublevel">
                                 .auto
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                🔃 Dirección
+                            </li>
+                            <li className="sublevel">
+                                .row
+                            </li>
+                            <li className="sublevel">
+                                .column
+                            </li>
+                            <li className="sublevel">
+                                .row-responsive
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                🔛 Justificación
+                            </li>
+                            <li className="sublevel">
+                                .justify-start
+                            </li>
+                            <li className="sublevel">
+                                .justify-center
+                            </li>
+                            <li className="sublevel">
+                                .justify-end
+                            </li>
+                            <li className="sublevel">
+                                .justify-between
+                            </li>
+                            <li className="sublevel">
+                                .justify-arround
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                🔝 Alineación
+                            </li>
+                            <li className="sublevel">
+                                .align-start
+                            </li>
+                            <li className="sublevel">
+                                .align-center
+                            </li>
+                            <li className="sublevel">
+                                .align-end
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                🔣 Agrupación
+                            </li>
+                            <li className="sublevel">
+                                .wap
+                            </li>
+                            <li className="sublevel">
+                                .nowrap
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level">
+                        ⏸️ Espacios en blanco
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                🖼️ Imágenes
+                            </li>
+                            <li className="sublevel">
+                                .responsive-img
+                            </li>
+                            <li className="sublevel">
+                                .cover-img
+                            </li>
+                            <li className="sublevel">
+                                .contain-img
+                            </li>
+                        </ul>
+                    </li>
+                    <li className="level-container">
+                        <ul>
+                            <li className="level">
+                                📝 Decoradores de texto
+                            </li>
+                            <li className="sublevel">
+                                .text-left
+                            </li>
+                            <li className="sublevel">
+                                .text-center
+                            </li>
+                            <li className="sublevel">
+                                .text-right
+                            </li>
+                            <li className="sublevel">
+                                .text-justify
                             </li>
                         </ul>
                     </li>
